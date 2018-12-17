d692ef67c9228409c2c6877178b6c577f6be0a7d
Ilya Subota <ilyasubota777@gmail.com>
2018-12-17 04:21:20 +0300

    Some additions

--- script.js
+++ script.js
@@ -76,19 +76,21 @@ var Cart = [];
 const Roudes = {
 	"#home": {
 		path: "home.html",
-		// handler: function taimer(){
-		// 		var clock = document.getElementById("taim");
-		// 		var date = new Date();
-		// 		var hours = 23 - date.getHours();
-		// 			if (hours<10) hours = "0"+hours;
-		// 		var minutes = 59 - date.getMinutes();
-		// 			if (minutes<10) minutes = "0"+minutes;
-		// 		var seconds = 60 - date.getSeconds();
-		// 			if (seconds<10) seconds = "0"+seconds;
-		// 		document.getElementById("taim").innerHTML=hours+points+minutes+points+seconds; 
-		// 			points = points == ':' ? ' ' : ':';
-		// 		setTimeout("taimer()",500); 
-		// }
+		handler: function(){
+			taimer = function(){
+				var clock = document.getElementById("taim");
+				var date = new Date();
+				var hours = 23 - date.getHours();
+					if (hours<10) hours = "0"+hours;
+				var minutes = 59 - date.getMinutes();
+					if (minutes<10) minutes = "0"+minutes;
+				var seconds = 60 - date.getSeconds();
+					if (seconds<10) seconds = "0"+seconds;
+				document.getElementById("taim").innerHTML=hours+points+minutes+points+seconds; 
+					points = points == ':' ? ' ' : ':'; 
+			}
+			setInterval("taimer()",500);
+		}
 	},
 	"#katalog" : {
 		path: "katalog.html",
